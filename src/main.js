21d4e0bb21cf7ebde777efdfe37474b6f4e405f5
Rob Marissen <50015273+524D@users.noreply.github.com>
2021-02-22 18:29:39 +0100

    Resize main window

--- src/main.js
+++ src/main.js
@@ -96,8 +96,8 @@ Menu.setApplicationMenu(menu)
 const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
