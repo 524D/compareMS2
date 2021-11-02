6be505893eb877b6cf6acf540759e84b717a040c
Rob Marissen <r.j.marissen@lumc.nl>
2021-11-02 16:31:43 +0100

    Move advanced settings to separate tab

--- src/index.js
+++ src/index.js
@@ -93,7 +93,28 @@ function updateMgfInfo(path) {
   document.getElementById("submit").disabled = (nMgf<2);
 }
 
-// *******************************start of initialization ******************************************** //
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+  // *******************************start of initialization ******************************************** //
 
 // Set defaults for input fields
 setOptions(defaultOptions);
