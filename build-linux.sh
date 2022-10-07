1c11c0ee40aff5b4d3e7d02e0ba6bcb87721cae0
Rob Marissen <r.j.marissen@lumc.nl>
2022-10-07 14:43:12 +0200

    Correct file naming in Linux build

--- build-linux.sh
+++ build-linux.sh
@@ -4,7 +4,7 @@ cp package.json.keep package.json
 perl -pi -e 's/"name": "compare-ms2"/"name": "comparems2"/g; s/"productName": "compareMS2"/"productName": "comparems2"/g; ' package.json
 electron-forge make
 
-# Find the line with verion number, replace "-" by "~", and rename the .deb file
-perl -n -e 'if (m/"version": "([^"]*)"/g) { $v=$1 ; ($v =~ s/-/~/ ) ; rename("./out/make/comparems2_${v}_amd64.deb", "./out/make/compareMS2_${v}_amd64.deb")}' package.json
+# Find the line with version number, replace "-" by "~", and rename the .deb file
+perl -n -e 'if (m/"version": "([^"]*)"/g) { $v=$1 ; rename("./out/make/comparems2_${v}_amd64.deb", "./out/make/compareMS2-${v}_amd64.deb")}' package.json
 
 mv package.json.keep package.json
