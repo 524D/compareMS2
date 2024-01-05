1e0ba16228116dc7712017e7dc97c05d29b76c75
Rob Marissen <r.j.marissen@lumc.nl>
2024-01-05 16:56:30 +0100

    Add initial code for context separation

--- src/preload.js
+++ src/preload.js
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: MIT
 // Copyright 2022 Rob Marissen.
 
-// All of the Node.js APIs are available in the preload process.
+// FIXME: enable context isolation by implementing all relevant functions in preload.js and
+//  uncommenting below
+
+// const { contextBridge, ipcRenderer } = require('electron/renderer')
+
+// // Expose protected methods that allow the renderer process to use
+// // FIXME: All communication with the main process should be done this way
+// contextBridge.exposeInMainWorld('electronAPI', {
+//     storeImage: (imgFmt, imageData, instanceId) => ipcRenderer.send('store-image', imgFmt, imageData, instanceId)
+// })
 
 window.addEventListener('DOMContentLoaded', () => {
 })
